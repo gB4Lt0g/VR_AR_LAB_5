21470d7f3c767ac8d1c7a4c58a23cef4e6d4aa60
gB4lt0g <gpoloten4ikg@gmail.com>
2025-11-22 18:49:48 +0200

    Done

---
+++ test1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,36 @@
+﻿#include "pch-c.h"
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+extern const int32_t* g_FieldOffsetTable[];
+extern const Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	6448,
+	g_Il2CppGenericTypes,
+	4083,
+	g_Il2CppGenericInstTable,
+	55699,
+	g_Il2CppGenericMethodFunctions,
+	18874,
+	g_Il2CppTypeTable,
+	64859,
+	g_Il2CppMethodSpecTable,
+	5578,
+	g_FieldOffsetTable,
+	5578,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
